08f535ec421a8dbf82da7d5da7c953f2872f3e29
sanket <sanketm221995@gmail.com>
2024-12-13 21:57:37 +0530

    Added homebrew package implementation

--- startgen.rb
+++ startgen.rb
@@ -1,9 +1,9 @@
 class Startgen < Formula
   desc "A CLI tool to generate boilerplate projects using natural language"
   homepage "https://github.com/mario2334/startgen"
-  url "https://github.com/mario2334/startgen/releases/download/v0.1.1-alpha/startgen-darwin-amd64.zip" # Update for your release
-  sha256 "19f5e0da672f8cfb4684a2cb68e8b929e56f3f573abaf903121eea5fec452f84" # Calculate using 'shasum -a 256 <file>'
-  version "1.0.0"
+  url "https://github.com/Mario2334/startgen/releases/download/v0.1.1-alpha/startgen-darwin-amd64.zip" # Update for your release
+  sha256 "be17add25d763c22bd8e1487406b3e2fa4ee61f61cd23199f9d7360478d4f375" # Calculate using 'shasum -a 256 <file>'
+  version "0.1.1-alpha"
 
   def install
     bin.install "startgen"
