13ef64540c94a08fb407325731a4c13c088b6fed
sanket <sanketm221995@gmail.com>
2024-12-13 21:48:26 +0530

    Added go implementation due to speed issue on python

--- startgen.rb
+++ startgen.rb
@@ -1,7 +1,7 @@
 class Startgen < Formula
   desc "A CLI tool to generate boilerplate projects using natural language"
   homepage "https://github.com/mario2334/startgen"
-  url "https://github.com/mario2334/startgen/releases/download/v0.1.0-alpha/startgen-darwin-amd64.zip" # Update for your release
+  url "https://github.com/mario2334/startgen/releases/download/v0.1.1-alpha/startgen-darwin-amd64.zip" # Update for your release
   sha256 "19f5e0da672f8cfb4684a2cb68e8b929e56f3f573abaf903121eea5fec452f84" # Calculate using 'shasum -a 256 <file>'
   version "1.0.0"
 
